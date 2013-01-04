3a35619d520bf7efcdf3f9751fe26bf7305fb4f5
mvladm14 <mvladm14@yahoo.com>
2013-01-04 12:56:38 -0800

    mail

--- EduIncluziva_01/EduIncluziva/Models/EmailDetail.cs
+++ EduIncluziva_01/EduIncluziva/Models/EmailDetail.cs
@@ -7,18 +7,10 @@ namespace EduIncluziva.Models
 {
     public class EmailDetail
     {
-       
-        public EmailDetail(string nume, string prenume, string bio, string mail)
-        {
-            // TODO: Complete member initialization
-            this.nume = nume;
-            this.prenume = prenume;
-            this.bio = bio;
-            this.mail = mail;
-        }
         public string nume { get; set; }
         public string mail { get; set; }
         public string bio { get; set; }
         public string prenume { get; set; }
+        public string pass { get; set; }
     }
 }
