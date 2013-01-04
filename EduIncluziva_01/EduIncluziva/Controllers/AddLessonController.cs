15a449d85ac77bddd23dbefb882b3254afd90309
mvladm14 <mvladm14@yahoo.com>
2013-01-04 04:39:12 -0800

    File Uppload La lectie

--- EduIncluziva_01/EduIncluziva/Controllers/AddLessonController.cs
+++ EduIncluziva_01/EduIncluziva/Controllers/AddLessonController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.IO;
 
 namespace EduIncluziva.Controllers
 {
@@ -14,7 +15,21 @@ namespace EduIncluziva.Controllers
         {
             return View();
         }
-
+        [HttpPost]
+        public ActionResult AddLesson(HttpPostedFileBase file)
+        {
+            // Verify that the user selected a file
+            if (file != null && file.ContentLength > 0)
+            {
+                // extract only the fielname
+                var fileName = Path.GetFileName(file.FileName);
+                // store the file inside ~/App_Data/uploads folder
+                var path = Path.Combine(Server.MapPath("~/App_Data/uploads"), fileName);
+                file.SaveAs(path);
+            }
+            // redirect back to the index action to show the form once again
+            return View("../../Views/Profesori/PaginaProfesorului");
+        }
         public ActionResult Index()
         {
             return View();
