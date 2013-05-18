8ffad80c187be3b86c1ebd4427edab2cfc29efb6
mvladm14 <mvladm14@yahoo.com>
2013-05-18 03:13:45 -0700

    pt inregistrare profesor

---
+++ EduIncluziva_01/EduIncluziva/Models/InregistrareProfesorModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EduIncluziva.Models
+{
+    public class InregistrareProfesorModel
+    {
+        [Required]
+        [Display(Name = "Nume")]
+        public string Nume { get; set; }
+
+        [Required]
+        [Display(Name = "Prenume")]
+        public string Prenume { get; set; }
+
+        [Required]
+        [Display(Name = "Adresa de mail")]
+        [DataType(DataType.EmailAddress)]
+        public string Mail { get; set; }
+
+        [Required]
+        [Display(Name = "Scoala de provenienta")]
+        public string ScoalaDeProveninenta { get; set; }
+
+        [Required]
+        [Display(Name = "Descriere")]
+        public string Descriere { get; set; }       
+
+        [Required]
+        [StringLength(100, ErrorMessage = "{0} trebuie sa contina cel putin {2} caractere.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Parola")]
+        public string Parola { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirmare parola")]
+        [Compare("Parola", ErrorMessage = "Parolele nu coincid.")]
+        public string ConfirmareParola { get; set; }
+    }
+}
